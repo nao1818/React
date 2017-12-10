a1fed726fb5c29b350dfe57d4c94eff9ca7c9df1
nao-18 <kyahyo17@yahoo.co.jp>
2017-12-11 06:34:08 +0900

    3-19

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Greeting from './greeting';
 
@@ -8,13 +8,35 @@ import Greeting from './greeting';
 //     }
 //   }
 
-function App() {
-  return (
-    <div>
-      <Greeting name="BOb" />
-      <Greeting name="john" />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: 'Arai',
+    };
+  }
+
+  handleNameChange(e) {
+    this.setState({ name: e.target.value });
+  }
+
+  handleNameBob() {
+    this.setState({ name: 'Bob' });
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="text"
+          value={this.state.name}
+          onChange={e => this.handleNameChange(e)}
+        />
+        <button onClick={() => this.handleNameBob()}>I am Bob</button>
+        <Greeting name={this.state.name} />
+      </div>
+    );
+  }
 }
 
 
